35c4960500d2b17abdf73a5c230dc7dcb7c03739
Sirjeleshka <sirjeleshka@gmail.com>
2021-04-12 18:43:10 +0300

    Deleted nan from TestDriver

--- lab08/prj/TestDriver/main.cpp
+++ lab08/prj/TestDriver/main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main()
 {
-    float x_val[] = {1,8,0,-1,17,25,71,-0.9};
-    float z_val[] = {1,4,3,25,-5,25,100,16};
+    float x_val[] = {1,8,9,25,71,-0.9};
+    float z_val[] = {1,4,7,25,100,16};
 
-    int S[] = {1,2,0,0,0,5,10,7};
+    int S[] = {1,2,3,5,10,7};
 
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < 6; i++)
     {
         int result = s_calculation(x_val[i],z_val[i]);
         cout << i+1 << '.' << "x = " << x_val[i] << ", y = " << z_val[i];
-        if(result == S[i] || (result != S[i] && S[i] == 0))
+        if(result == S[i])
         {
             cout  << ": Passed" << endl;
         }
